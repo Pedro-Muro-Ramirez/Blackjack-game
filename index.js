449bcc223b0ea328744e5938412eaece850d702b
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-09-03 18:35:09 -0500

    Adding Render and New Card Functions

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 //Initial variables 
-let sum = 0; 
-let cards = []
-let sumEl = document.getElementById("sum-el"); 
-let messageEl = document.getElementById("message-el"); 
-let cardsEl = document.getElementById("cards-el"); 
+let sum = 0
+let cards = [] 
+let sumEl = document.getElementById("sum-el")
+let messageEl = document.getElementById("message-el") 
+let cardsEl = document.getElementById("cards-el")
+
+
+// Random Number Function
+function randomNumber() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 // Initiate game function 
 function startGame() {
-    let firstCard = Math.floor(Math.random() * 13); 
-    let secondCard = Math.floor(Math.random() * 13); 
-    let sum = firstCard + secondCard
+    let firstCard = randomNumber() 
+    let secondCard = randomNumber() 
+    sum = firstCard + secondCard
+    cards = [firstCard, secondCard]
+    renderText()
+     
+}
 
-    cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`
-    sumEl.textContent = `Sum: ${sum}`
-
-    if (sum > 21) {
+// Render Game text function
+function renderText() {
+    cardsEl.textContent = `Cards: `
+    sumEl.textContent = `Sum: `
+    let total = 0
+    for (let i = 0; i < cards.length; i++) {
+         cardsEl.textContent += cards[i] + " "
+         total += cards[i]
+         sumEl.textContent = `Sum: ${total}`
+    }
+     if (sum > 21) {
         messageEl.textContent = `You're out of the game`
     } else if (sum < 21) {
         messageEl.textContent = `Do you want to draw a new card?`
     } else {
         messageEl.textContent = `You've won!`
     }
+}
 
+// Add new card function 
+function newCard() {
+    let randomNum = randomNumber()
+    cards.push(randomNum)
+    renderText()
 }
